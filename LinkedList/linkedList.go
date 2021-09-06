e965f458f4df0ce2100cd9befcb2142e204195d0
SinaKhorsandi <sina76khorsandi@gmail.com>
2021-09-06 17:50:26 +0430

    Create LinkedList package in Go

--- LinkedList/linkedList.go
+++ LinkedList/linkedList.go
@@ -1,1 +1,69 @@
 package LinkedList
+
+type LinkedList struct {
+	First *Node
+	Last  *Node
+}
+
+type Node struct {
+	Next *Node
+	Data interface{}
+}
+
+func New() *LinkedList {
+	emptyNode := &Node{
+		Next: nil,
+		Data: nil,
+	}
+	return &LinkedList{
+		First: emptyNode,
+		Last:  emptyNode,
+	}
+}
+
+// Add Function : Add a new node to the end of the Linked List
+func (ll *LinkedList) Add(d interface{}) *LinkedList {
+	NextNode:=&Node{
+		Next: nil,
+		Data: d,
+	}
+	if ll.First == nil {
+		ll.First = NextNode
+		return ll
+	}else {
+		ll.Last.Next=NextNode
+		return ll
+	}
+	ll.Last = NextNode
+	return ll
+}
+
+// DeleteNode : Deletes node which has a specific value
+func (ll *LinkedList) DeleteNode(d interface{}) *LinkedList {
+	var node = ll.First
+	if node.Data == d {
+		node = node.Next
+		return ll
+	}
+	for {
+		if node.Next == d {
+			if node.Next.Next != nil {
+				node.Next = node.Next.Next
+				break
+			} else {
+				node.Next = nil
+			}
+		}
+		node = node.Next
+	}
+	return ll
+}
+
+
+// IsEmpty : check if there are any elements in the list
+func (ll *LinkedList) IsEmpty() bool {
+	if ll.First == nil {
+		return true
+	}
+	return false
+}
